150dd082209595e53f898c1a3fe2b1a5aa58d33c
Kampsync <bookercampers@gmail.com>
2025-05-31 18:54:26 -0600

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('/calendar/:listingId.ics', async (req, res) => {
 
     // Set headers and send the calendar
     res.setHeader('Content-Type', 'text/calendar; charset=utf-8');
-    res.setHeader('Content-Disposition', 'inline');
+    res.setHeader('Content-Disposition', `attachment; filename="listing-${listingId}.ics"`);
     res.send(calendar.toString());
   } catch (error) {
     console.error('Error fetching booking events:', error);
