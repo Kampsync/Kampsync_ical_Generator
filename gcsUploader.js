1d6b82032d47e01f1ead1e1c9f4212b748fb7e80
Kampsync <bookercampers@gmail.com>
2025-06-10 22:03:38 -0600

    Update gcsUploader.js

--- gcsUploader.js
+++ gcsUploader.js
@@ -3,14 +3,15 @@ const { Storage } = require('@google-cloud/storage');
 
 const storage = new Storage();
 const bucketName = process.env.GCLOUD_BUCKET;
+const bucket = storage.bucket(bucketName);
 
 async function uploadToGCS(filename, contents) {
-  const bucket = storage.bucket(bucketName);
   const file = bucket.file(filename);
-  await file.save(contents, {
+
+  await file.save(Buffer.from(contents, 'utf8'), {
     contentType: 'text/calendar',
     public: true,
-    resumable: false
+    resumable: false,
   });
 }
 
